03378263e426d4dad6abe599b2cb886fa21b0544
evgenij <mwbox89@gmail.com>
2022-11-06 16:29:40 +0200

    first commit

---
+++ app/js/app.js
@@ -0,0 +1,82 @@
+// // Import vendor jQuery plugin example
+// import '~/app/libs/mmenu/dist/mmenu.js'
+
+document.addEventListener('DOMContentLoaded', () => {
+	// Custom JS
+	function accordionInit (list) {
+		let wrap = list
+		if (!wrap) {
+			return
+		}
+		wrap.addEventListener('click', function (e) {
+			if(e.target.classList.contains('accordion__item-title')) {
+				let openedElement = document.querySelector('.accordion__item.open')
+				openedElement.classList.remove('open')
+				e.target.parentElement.classList.add('open')
+			}
+		})
+	}
+	accordionInit (document.querySelector('.accordion'))
+
+	function blogPreviwSlider () {
+		let swiper = new Swiper(".blog-preview__slider", {
+			slidesPerView: 3,
+			spaceBetween: 24,
+			pagination: {
+			  clickable: true,
+			},
+			breakpoints: {
+				320: {
+					slidesPerView: 1
+				  },
+				  768: {
+					slidesPerView: 2
+				  },
+				  992: {
+					slidesPerView: 3
+				  }
+			}
+		  });
+	}
+	blogPreviwSlider ()
+	function mobMenuToggle () {
+		let btn = document.querySelector('.header__navigation-btn-menu')
+		let menu = document.querySelector(btn.dataset.toggle)
+			btn.addEventListener('click', function (e) {
+				btn.classList.toggle('active')
+				menu.classList.toggle('active')
+			})
+	}
+	mobMenuToggle ()
+
+	function blogCategoryToggle () {
+		let categories = document.querySelector('.blog__categories')
+		if (!categories) {
+			return
+		}
+		let articles = document.querySelectorAll('.article-thumbnail')
+		let activeCategory = document.querySelector('.blog__categories-btn.active').dataset.category
+		categories.addEventListener('click', function (e) {
+			if (e.target.classList.contains('blog__categories-btn')) {
+				document.querySelector('.blog__categories-btn.active').classList.remove('active')
+				e.target.classList.add('active')
+				activeCategory = e.target.dataset.category
+
+				sortArticles (activeCategory, articles)
+			}
+		})
+
+		function sortArticles (category, articles) {
+			for (let i = 0; i < articles.length; i++) {
+				console.log(articles.length)
+				if ((category === articles[i].dataset.category) || category === 'all') {
+					articles[i].parentElement.style.display = ""
+				} else {
+					articles[i].parentElement.style.display = "none"
+				}
+			}
+		}
+	}
+
+	blogCategoryToggle ()
+})
